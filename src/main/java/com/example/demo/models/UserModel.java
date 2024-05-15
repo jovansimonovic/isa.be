970b276293df6ad67500ec7ab9a0550b9d56aa94
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-05-15 14:12:03 +0200

    Kod sa vezbi 13.05.

--- src/main/java/com/example/demo/models/UserModel.java
+++ src/main/java/com/example/demo/models/UserModel.java
@@ -1,10 +1,14 @@
 package com.example.demo.models;
 
+import com.example.demo.validators.ContactNumberConstraint;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserModel {
   private int id;
   @NotBlank
@@ -12,5 +16,6 @@ public class UserModel {
   private String lastName;
   @Email
   private String email;
+  @ContactNumberConstraint
   private String contactNumber;
 }
